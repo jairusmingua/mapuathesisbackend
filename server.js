776c20483a5d2a08118adffc6d211e3013a4ef79
Jairus <minguajairus@yahoo.com.ph>
2020-09-24 00:32:53 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const { MongoClient } = require("mongodb");
+const dotenv = require("dotenv").config();
+const client = new MongoClient(process.env.URI);
+const cors = require("cors");
+app.use(cors());
+var collection = null;
+app.listen(5000, async () => {
+  try {
+    await client.connect();
+    console.log("App listening");
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.get("/", async (req, res) => {
+  try {
+    var database = client.db("thesis");
+    collection = database.collection("tweets");
+    const tweet = await collection.aggregate(
+        [ { $sample: { size: 1 } } ]
+    ).toArray();
+    res.send(tweet);
+} catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
