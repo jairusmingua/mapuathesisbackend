4a0b2edf43ae18de1b9eb5b669618ecf5a4beac1
Jairus <minguajairus@yahoo.com.ph>
2020-09-28 00:54:33 +0800

    added post capabilities

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const { MongoClient } = require("mongodb");
+const mongo = require("mongodb");
 const dotenv = require("dotenv").config();
 const client = new MongoClient(process.env.URI);
 const cors = require("cors");
 app.use(cors());
-var collection = null;
+app.use(express.json());
+
+
+const _database = "thesis";
+const _collection = "tweets";
+
 app.listen(process.env.PORT||5000, async () => {
   try {
     await client.connect();
@@ -16,8 +22,8 @@ app.listen(process.env.PORT||5000, async () => {
 });
 app.get("/", async (req, res) => {
   try {
-    var database = client.db("thesis");
-    collection = database.collection("tweets");
+    let database = client.db(_database);
+    let collection = database.collection(_collection);
     const tweet = await collection.aggregate(
         [ { $sample: { size: 1 } } ]
     ).toArray();
@@ -25,5 +31,24 @@ app.get("/", async (req, res) => {
 } catch (error) {
     console.log(error);
     res.send(error);
+  }
+});
+app.post("/", async(req,res)=>{
+  let id = req.body._id;
+  let answer = req.body.answer;
+  let respondent = req.body.respondent;
+  try {
+    let database = client.db(_database);
+    let collection = database.collection(_collection);
+    collection.updateOne({_id:new mongo.ObjectID(id)},{
+      $set:{label:answer,respondent:respondent,responded:true}
+    }).then((data)=>{
+      res.send(data);
+    }).catch((error)=>{
+      res.send(error);
+    })
+  } catch (error) {
+    console.log(error);
+    res.send(error).status(401);
   }
 });
